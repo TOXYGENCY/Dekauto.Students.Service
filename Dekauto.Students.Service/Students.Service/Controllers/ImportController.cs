7984a02e00383555703a519187819be71a46d63c
TOXYGENCY <delorean2026@ya.ru>
2025-03-21 23:20:11 +0300

    Тесты для ImportController

--- Dekauto.Students.Service/Students.Service/Controllers/ImportController.cs
+++ Dekauto.Students.Service/Students.Service/Controllers/ImportController.cs
@@ -8,16 +8,14 @@ namespace Dekauto.Students.Service.Students.Service.Controllers
     [ApiController]
     public class ImportController : ControllerBase
     {
-        private readonly IConfiguration configuration;
         private readonly IImportProvider importProvider;
-        public ImportController(IConfiguration configuration, IImportProvider importProvider)
+        public ImportController(IImportProvider importProvider)
         {
-            this.configuration = configuration;
             this.importProvider = importProvider;
         }
 
         [HttpPost]
-        public async Task<ActionResult> ImportFilesFromFrontend([FromForm] ImportFilesAdapter files)
+        public async Task<ActionResult> ImportFilesFromFrontendAsync([FromForm] ImportFilesAdapter files)
         {
             try
             {
--- Students.Tests/StudentsControllerTests.cs
+++ Students.Tests/StudentsControllerTests.cs
@@ -11,31 +11,31 @@ namespace Students.Tests;
 [TestClass]
 public sealed class StudentsControllerTests
 {
-    private Mock<IStudentsService> _studentsServiceMock;
-    private Mock<IStudentsRepository> _studentsRepositoryMock;
-    private StudentsController _studentsController;
+    private Mock<IStudentsService> studentsServiceMock;
+    private Mock<IStudentsRepository> studentsRepositoryMock;
+    private StudentsController studentsController;
 
 
     [TestInitialize]
     public void Setup()
     {
-        _studentsServiceMock = new Mock<IStudentsService>();
-        _studentsRepositoryMock = new Mock<IStudentsRepository>();
+        studentsServiceMock = new Mock<IStudentsService>();
+        studentsRepositoryMock = new Mock<IStudentsRepository>();
     }
 
     [TestMethod]
     public async Task GetAllStudents_Valid_Ok()
     {
         // Arrange
-        _studentsRepositoryMock.Setup(x => x.GetAllAsync())
+        studentsRepositoryMock.Setup(x => x.GetAllAsync())
             .ReturnsAsync(new List<Student>());
-        _studentsServiceMock.Setup(x => x.ToDtos(It.IsAny<List<Student>>()))
+        studentsServiceMock.Setup(x => x.ToDtos(It.IsAny<List<Student>>()))
             .Returns(new List<StudentDto>());
 
-        _studentsController = new StudentsController(_studentsRepositoryMock.Object, _studentsServiceMock.Object);
+        studentsController = new StudentsController(studentsRepositoryMock.Object, studentsServiceMock.Object);
 
         // Act
-        var response = await _studentsController.GetAllStudents();
+        var response = await studentsController.GetAllStudents();
 
         // Assert
         Assert.IsInstanceOfType<OkObjectResult>(response.Result);
@@ -45,13 +45,13 @@ public sealed class StudentsControllerTests
     public async Task GetAllStudents_RepoExcept_Status500()
     {
         // Arrange
-        _studentsRepositoryMock.Setup(x => x.GetAllAsync())
+        studentsRepositoryMock.Setup(x => x.GetAllAsync())
             .ThrowsAsync(new Exception());
 
-        _studentsController = new StudentsController(_studentsRepositoryMock.Object, _studentsServiceMock.Object);
+        studentsController = new StudentsController(studentsRepositoryMock.Object, studentsServiceMock.Object);
 
         // Act
-        var response = await _studentsController.GetAllStudents();
+        var response = await studentsController.GetAllStudents();
 
         // Assert
         Assert.AreEqual(StatusCodes.Status500InternalServerError, (response.Result as ObjectResult).StatusCode);
@@ -61,15 +61,15 @@ public sealed class StudentsControllerTests
     public async Task GetAllStudents_ServExcept_Status500()
     {
         // Arrange
-        _studentsRepositoryMock.Setup(x => x.GetAllAsync())
+        studentsRepositoryMock.Setup(x => x.GetAllAsync())
             .ReturnsAsync(new List<Student>());
-        _studentsServiceMock.Setup(x => x.ToDtos(It.IsAny<List<Student>>()))
+        studentsServiceMock.Setup(x => x.ToDtos(It.IsAny<List<Student>>()))
             .Throws(new Exception());
 
-        _studentsController = new StudentsController(_studentsRepositoryMock.Object, _studentsServiceMock.Object);
+        studentsController = new StudentsController(studentsRepositoryMock.Object, studentsServiceMock.Object);
 
         // Act
-        var response = await _studentsController.GetAllStudents();
+        var response = await studentsController.GetAllStudents();
 
         // Assert
         Assert.AreEqual(StatusCodes.Status500InternalServerError, (response.Result as ObjectResult).StatusCode);
@@ -82,15 +82,15 @@ public sealed class StudentsControllerTests
     {
         // Arrange
         var id = new Guid();
-        _studentsRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>()))
+        studentsRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>()))
             .ReturnsAsync(new Student());
-        _studentsServiceMock.Setup(x => x.ToDto(It.IsAny<Student>()))
+        studentsServiceMock.Setup(x => x.ToDto(It.IsAny<Student>()))
             .Returns(new StudentDto());
 
-        _studentsController = new StudentsController(_studentsRepositoryMock.Object, _studentsServiceMock.Object);
+        studentsController = new StudentsController(studentsRepositoryMock.Object, studentsServiceMock.Object);
 
         // Act
-        var response = await _studentsController.GetStudentById(id);
+        var response = await studentsController.GetStudentById(id);
 
         // Assert
         Assert.IsInstanceOfType<OkObjectResult>(response.Result);
@@ -101,13 +101,13 @@ public sealed class StudentsControllerTests
     {
         // Arrange
         var id = new Guid();
-        _studentsRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>()))
+        studentsRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>()))
             .ThrowsAsync(new Exception());
 
-        _studentsController = new StudentsController(_studentsRepositoryMock.Object, _studentsServiceMock.Object);
+        studentsController = new StudentsController(studentsRepositoryMock.Object, studentsServiceMock.Object);
 
         // Act
-        var response = await _studentsController.GetStudentById(id);
+        var response = await studentsController.GetStudentById(id);
 
         // Assert
         Assert.AreEqual(StatusCodes.Status500InternalServerError, (response.Result as ObjectResult).StatusCode);
@@ -118,15 +118,15 @@ public sealed class StudentsControllerTests
     {
         // Arrange
         var id = new Guid();
-        _studentsRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>()))
+        studentsRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>()))
             .ReturnsAsync(new Student());
-        _studentsServiceMock.Setup(x => x.ToDto(It.IsAny<Student>()))
+        studentsServiceMock.Setup(x => x.ToDto(It.IsAny<Student>()))
             .Throws(new Exception());
 
-        _studentsController = new StudentsController(_studentsRepositoryMock.Object, _studentsServiceMock.Object);
+        studentsController = new StudentsController(studentsRepositoryMock.Object, studentsServiceMock.Object);
 
         // Act
-        var response = await _studentsController.GetStudentById(id);
+        var response = await studentsController.GetStudentById(id);
 
         // Assert
         Assert.AreEqual(StatusCodes.Status500InternalServerError, (response.Result as ObjectResult).StatusCode);
@@ -137,13 +137,13 @@ public sealed class StudentsControllerTests
     {
         // Arrange
         var id = new Guid();
-        _studentsRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>()))
+        studentsRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>()))
             .ReturnsAsync((Student)null);
 
-        _studentsController = new StudentsController(_studentsRepositoryMock.Object, _studentsServiceMock.Object);
+        studentsController = new StudentsController(studentsRepositoryMock.Object, studentsServiceMock.Object);
 
         // Act
-        var response = await _studentsController.GetStudentById(id);
+        var response = await studentsController.GetStudentById(id);
 
         // Assert
         Assert.AreEqual(StatusCodes.Status404NotFound, (response.Result as StatusCodeResult).StatusCode);
@@ -157,12 +157,12 @@ public sealed class StudentsControllerTests
         // Arrange
         var id = new Guid();
         var updatedStudentDto = new StudentDto();
-        _studentsServiceMock.Setup(x => x.UpdateAsync(It.IsAny<Guid>(), It.IsAny<StudentDto>()));
+        studentsServiceMock.Setup(x => x.UpdateAsync(It.IsAny<Guid>(), It.IsAny<StudentDto>()));
 
-        _studentsController = new StudentsController(_studentsRepositoryMock.Object, _studentsServiceMock.Object);
+        studentsController = new StudentsController(studentsRepositoryMock.Object, studentsServiceMock.Object);
 
         // Act
-        var response = await _studentsController.UpdateStudentAsync(id, updatedStudentDto);
+        var response = await studentsController.UpdateStudentAsync(id, updatedStudentDto);
 
         // Assert
         Assert.IsInstanceOfType<OkResult>(response);
@@ -174,13 +174,13 @@ public sealed class StudentsControllerTests
         // Arrange
         var id = new Guid();
         var updatedStudentDto = new StudentDto();
-        _studentsServiceMock.Setup(x => x.UpdateAsync(It.IsAny<Guid>(), It.IsAny<StudentDto>()))
+        studentsServiceMock.Setup(x => x.UpdateAsync(It.IsAny<Guid>(), It.IsAny<StudentDto>()))
             .ThrowsAsync(new Exception());
 
-        _studentsController = new StudentsController(_studentsRepositoryMock.Object, _studentsServiceMock.Object);
+        studentsController = new StudentsController(studentsRepositoryMock.Object, studentsServiceMock.Object);
 
         // Act
-        var response = await _studentsController.UpdateStudentAsync(id, updatedStudentDto);
+        var response = await studentsController.UpdateStudentAsync(id, updatedStudentDto);
 
         // Assert
         Assert.AreEqual(StatusCodes.Status500InternalServerError, (response as ObjectResult).StatusCode);
@@ -193,12 +193,12 @@ public sealed class StudentsControllerTests
     {
         // Arrange
         var studentDto = new StudentDto();
-        _studentsServiceMock.Setup(x => x.AddAsync(It.IsAny<StudentDto>()));
+        studentsServiceMock.Setup(x => x.AddAsync(It.IsAny<StudentDto>()));
 
-        _studentsController = new StudentsController(_studentsRepositoryMock.Object, _studentsServiceMock.Object);
+        studentsController = new StudentsController(studentsRepositoryMock.Object, studentsServiceMock.Object);
 
         // Act
-        var response = await _studentsController.AddStudentAsync(studentDto);
+        var response = await studentsController.AddStudentAsync(studentDto);
 
         // Assert
         Assert.IsInstanceOfType<OkResult>(response);
@@ -209,13 +209,13 @@ public sealed class StudentsControllerTests
     {
         // Arrange
         var studentDto = new StudentDto();
-        _studentsServiceMock.Setup(x => x.AddAsync(It.IsAny<StudentDto>()))
+        studentsServiceMock.Setup(x => x.AddAsync(It.IsAny<StudentDto>()))
             .ThrowsAsync(new Exception());
 
-        _studentsController = new StudentsController(_studentsRepositoryMock.Object, _studentsServiceMock.Object);
+        studentsController = new StudentsController(studentsRepositoryMock.Object, studentsServiceMock.Object);
 
         // Act
-        var response = await _studentsController.AddStudentAsync(studentDto);
+        var response = await studentsController.AddStudentAsync(studentDto);
 
         // Assert
         Assert.AreEqual(StatusCodes.Status500InternalServerError, (response as ObjectResult).StatusCode);
@@ -228,12 +228,12 @@ public sealed class StudentsControllerTests
     {
         // Arrange
         var id = new Guid();
-        _studentsRepositoryMock.Setup(x => x.DeleteAsync(It.IsAny<Guid>()));
+        studentsRepositoryMock.Setup(x => x.DeleteAsync(It.IsAny<Guid>()));
 
-        _studentsController = new StudentsController(_studentsRepositoryMock.Object, _studentsServiceMock.Object);
+        studentsController = new StudentsController(studentsRepositoryMock.Object, studentsServiceMock.Object);
 
         // Act
-        var response = await _studentsController.DeleteStudent(id);
+        var response = await studentsController.DeleteStudent(id);
 
         // Assert
         Assert.IsInstanceOfType<OkResult>(response);
@@ -244,13 +244,13 @@ public sealed class StudentsControllerTests
     {
         // Arrange
         var id = new Guid();
-        _studentsRepositoryMock.Setup(x => x.DeleteAsync(It.IsAny<Guid>()))
+        studentsRepositoryMock.Setup(x => x.DeleteAsync(It.IsAny<Guid>()))
             .ThrowsAsync(new Exception());
 
-        _studentsController = new StudentsController(_studentsRepositoryMock.Object, _studentsServiceMock.Object);
+        studentsController = new StudentsController(studentsRepositoryMock.Object, studentsServiceMock.Object);
 
         // Act
-        var response = await _studentsController.DeleteStudent(id);
+        var response = await studentsController.DeleteStudent(id);
 
         // Assert
         Assert.AreEqual(StatusCodes.Status500InternalServerError, (response as ObjectResult).StatusCode);
